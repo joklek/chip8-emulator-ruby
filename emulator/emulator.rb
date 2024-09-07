b247b530044b7959b911d7d56206dcdcbd0f0125
joklek <jokubas.lekevicius@gmail.com>
2024-09-07 22:28:37 +0300

    Add missing instructions and tests for them

--- emulator/emulator.rb
+++ emulator/emulator.rb
@@ -5,9 +5,10 @@ require_relative 'display_buffer'
 module Emulator
   class Emulator
     MEMORY_SIZE = 4 * 1024
+    FONT_OFFSET = 0x50
     PC_START = 0x200
 
-    attr_reader :memory, :display_buffer, :program_counter, :general_registers, :index_register
+    attr_reader :memory, :display_buffer, :program_counter, :general_registers, :index_register, :stack, :sound_timer, :delay_timer, :pressed_key
 
     def initialize
       @program_counter = PC_START
@@ -16,6 +17,9 @@ module Emulator
       @general_registers = Array.new(16, 0)
       @memory = initial_memory
       @display_buffer = DisplayBuffer.new
+      @delay_timer = 0
+      @sound_timer = 0
+      @pressed_key = nil # TODO implement. Also this implementation assumes one keypress at a time
     end
 
     def load_data(data)
@@ -47,15 +51,68 @@ module Emulator
 
       if opcode == 0x00E0
         @display_buffer.reset
+      elsif opcode == 0x00EE
+        @program_counter = @stack.pop
       elsif first_nibble == 1
         @program_counter = opcode & 0x0FFF
+      elsif first_nibble == 2
+        @stack.push(@program_counter)
+        @program_counter = opcode & 0x0FFF
+      elsif [3, 4, 5, 9].include?(first_nibble)
+        expected_value = third_nibble << 4
+        expected_value |= [3, 4].include?(first_nibble) ? fourth_nibble : 0
+
+        real_value = @general_registers[second_nibble]
+        real_value_2 = @general_registers[third_nibble]
+
+        if (first_nibble == 3 && real_value == expected_value) ||
+          (first_nibble == 4 && real_value != expected_value) ||
+          (first_nibble == 5 && real_value == real_value_2) ||
+          (first_nibble == 9 && real_value != real_value_2)
+          @program_counter += 2
+        end
       elsif first_nibble == 6
         @general_registers[second_nibble] = opcode & 0x00FF
       elsif first_nibble == 7
         @general_registers[second_nibble] += opcode & 0x00FF
-      elsif first_nibble == 0x000A
+      elsif first_nibble == 8 && fourth_nibble == 0
+        @general_registers[second_nibble] = @general_registers[third_nibble]
+      elsif first_nibble == 8 && fourth_nibble == 1
+        @general_registers[second_nibble] |= @general_registers[third_nibble]
+      elsif first_nibble == 8 && fourth_nibble == 2
+        @general_registers[second_nibble] &= @general_registers[third_nibble]
+      elsif first_nibble == 8 && fourth_nibble == 3
+        @general_registers[second_nibble] ^= @general_registers[third_nibble]
+      elsif first_nibble == 8 && fourth_nibble == 4
+        @general_registers[second_nibble] += @general_registers[third_nibble]
+        @general_registers[0xF] = @general_registers[second_nibble] > 0xFF ? 1 : 0
+        @general_registers[second_nibble] &= 0xFF
+      elsif first_nibble == 8 && [5, 7].include?(fourth_nibble)
+        minuend = fourth_nibble == 5 ? @general_registers[second_nibble] : @general_registers[third_nibble]
+        subtrahend = fourth_nibble == 5 ? @general_registers[third_nibble] : @general_registers[second_nibble]
+
+        @general_registers[second_nibble] = (minuend - subtrahend) & 0xFF
+        @general_registers[0xF] = minuend > subtrahend ? 1 : 0
+      elsif first_nibble == 8 && [6, 0xE].include?(fourth_nibble)
+        # @general_registers[second_nibble] = @general_registers[third_nibble]
+        value = @general_registers[second_nibble]
+
+        if fourth_nibble == 6
+          @general_registers[second_nibble] = value >> 1
+          @general_registers[0xF] = value & 0x1
+        else
+          @general_registers[second_nibble] = (value << 1) & 0xFF
+          @general_registers[0xF] = value >> 7
+        end
+      elsif first_nibble == 0xA
         @index_register = opcode & 0x0FFF
-      elsif first_nibble == 0x000D
+      elsif first_nibble == 0xB
+        address = opcode & 0x0FFF
+        offset = @general_registers[0]
+        @program_counter = address + offset
+      elsif first_nibble == 0xC
+        @general_registers[second_nibble] = rand(0x00FF) & (opcode & 0x00FF)
+      elsif first_nibble == 0xD
         x = @general_registers[second_nibble] % 64
         y = @general_registers[third_nibble] % 32
         height = fourth_nibble
@@ -72,9 +129,72 @@ module Emulator
             end
           end
         end
+      elsif opcode & 0xF0FF == 0xE09E || opcode & 0xF0FF == 0xE0A1
+        variant = opcode & 0x00FF
+        key_from_register = @general_registers[second_nibble]
+
+        @program_counter += 2 if (variant == 0x9E && key_from_register == @pressed_key)
+        @program_counter += 2 if (variant == 0xA1 && key_from_register != @pressed_key)
+      elsif opcode & 0xF0FF == 0xF00A
+        if @pressed_key
+          @general_registers[second_nibble] = @pressed_key
+        else
+          puts "Waiting for keypress"
+          @program_counter -= 2
+        end
+      elsif opcode & 0xF0FF == 0xF007
+        @general_registers[second_nibble] = @delay_timer
+      elsif opcode & 0xF0FF == 0xF015
+        @delay_timer = @general_registers[second_nibble]
+      elsif opcode & 0xF0FF == 0xF018
+        @sound_timer = @general_registers[second_nibble]
+      elsif opcode & 0xF0FF == 0xF01E
+        @index_register = (@index_register + @general_registers[second_nibble]) & 0xFFFF
+      elsif opcode & 0xF0FF == 0xF029
+        @index_register = FONT_OFFSET + (second_nibble & 0x000F) * 5
+      elsif opcode & 0xF0FF == 0xF033
+        value = @general_registers[second_nibble]
+
+        @memory[@index_register] = value / 100
+        @memory[@index_register + 1] = (value / 10) % 10
+        @memory[@index_register + 2] = value % 10
+      elsif opcode & 0xF0FF == 0xF055
+        for i in 0..second_nibble
+          @memory[@index_register + i] = @general_registers[i]
+        end
+      elsif opcode & 0xF0FF == 0xF065
+        for i in 0..second_nibble
+          @general_registers[i] = @memory[@index_register + i]
+        end
       else
-        raise "Unknown opcode: #{opcode}"
+        puts "Unknown opcode: 0x#{opcode.to_s(16)}"
       end
+    end
+
+    def decrement_delay_timer
+      return unless @delay_timer > 0
+
+      @delay_timer -= 1
+    end
+
+    def decrement_sound_timer
+      return unless @sound_timer > 0
+
+      @sound_timer -= 1
+    end
+
+    def pressed_key=(key)
+      @pressed_key = key
+    end
+
+    def delay_timer=(key)
+      puts 'this should not be called in production code'
+      @delay_timer = key
+    end
+
+    def index_register=(key)
+      puts 'this should not be called in production code'
+      @index_register = key
     end
 
     def running?
@@ -105,7 +225,7 @@ module Emulator
     def initial_memory
       initial_memory = Array.new(MEMORY_SIZE, 0)
 
-      FONTS.each.with_index { |byte, index| initial_memory[0x50 + index] = byte }
+      FONTS.each.with_index { |byte, index| initial_memory[FONT_OFFSET + index] = byte }
 
       initial_memory
     end
--- spec/emulator/emulator_spec.rb
+++ spec/emulator/emulator_spec.rb
@@ -92,6 +92,23 @@ RSpec.describe Emulator::Emulator do
       end
     end
 
+    describe '0x00EE' do
+      let(:opcode) { 0x00EE }
+      let(:address_in_stack) { 0x1234 }
+
+      before do
+        emulator.stack.push(0xFFFF)
+        emulator.stack.push(0xFFFF)
+        emulator.stack.push(address_in_stack)
+      end
+
+      it 'sets PC to address from stack' do
+        subject
+
+        expect(emulator.program_counter).to eq(address_in_stack)
+      end
+    end
+
     describe '0x1XXX' do
       let(:opcode) { 0x1ABC }
 
@@ -99,6 +116,106 @@ RSpec.describe Emulator::Emulator do
         subject
 
         expect(emulator.program_counter).to eq(0x0ABC)
+      end
+    end
+
+    describe '0x2XXX' do
+      let(:opcode) { 0x2ABC }
+
+      it 'set the program counter to given address' do
+        subject
+
+        expect(emulator.program_counter).to eq(0x0ABC)
+      end
+
+      it 'adds current address to stack' do
+        subject
+
+        expect(emulator.stack.last).to eq(0x200)
+      end
+    end
+
+    describe '0x3XNN' do
+      let(:opcode) { 0x31AB }
+
+      before { emulator.general_registers[1] = 0xAB }
+
+      it 'skips next instruction if register value equals to NN' do
+        expect{ subject }.to change{ emulator.program_counter }.by(2)
+      end
+
+      context 'when register value does not equal to NN' do
+        before { emulator.general_registers[1] = 0xAC }
+
+        it 'does not skip next instruction' do
+          expect{ subject }.not_to change{ emulator.program_counter }
+        end
+      end
+    end
+
+    describe '0x4XNN' do
+      let(:opcode) { 0x41AB }
+
+      before { emulator.general_registers[1] = 0xAC }
+
+      it 'skips next instruction if register value does not equal to NN' do
+        expect{ subject }.to change{ emulator.program_counter }.by(2)
+      end
+
+      context 'when register value is equal to NN' do
+        before { emulator.general_registers[1] = 0xAB }
+
+        it 'does not skip next instruction' do
+          expect{ subject }.not_to change{ emulator.program_counter }
+        end
+      end
+    end
+
+    describe '0x5XY0' do
+      let(:opcode) { 0x51A0 }
+
+      before do
+        emulator.general_registers[0x1] = 0xAC
+        emulator.general_registers[0xA] = 0xAC
+      end
+
+      it 'skips next instruction if register values VX and VY are equal' do
+        expect{ subject }.to change{ emulator.program_counter }.by(2)
+      end
+
+      context 'when register values VX and VY are not equal' do
+        before do
+          emulator.general_registers[0x1] = 0xAB
+          emulator.general_registers[0xA] = 0xAC
+        end
+
+        it 'does not skip next instruction' do
+          expect{ subject }.not_to change{ emulator.program_counter }
+        end
+      end
+    end
+
+    describe '0x9XY0' do
+      let(:opcode) { 0x91A0 }
+
+      before do
+        emulator.general_registers[0x1] = 0xAC
+        emulator.general_registers[0xA] = 0xAB
+      end
+
+      it 'skips next instruction if register values VX and VY are not equal' do
+        expect{ subject }.to change{ emulator.program_counter }.by(2)
+      end
+
+      context 'when register values VX and VY are equal' do
+        before do
+          emulator.general_registers[0x1] = 0xAC
+          emulator.general_registers[0xA] = 0xAC
+        end
+
+        it 'does not skip next instruction' do
+          expect{ subject }.not_to change{ emulator.program_counter }
+        end
       end
     end
 
@@ -136,6 +253,210 @@ RSpec.describe Emulator::Emulator do
       end
     end
 
+    describe '0x8XY0' do
+      let(:opcode) { 0x8120 }
+
+      before { emulator.general_registers[2] = 0x00AB }
+
+      it "sets VX = VY" do
+        expect { subject }.to change { emulator.general_registers[1] }.to(0x00AB)
+      end
+    end
+
+    describe '0x8XY1' do
+      let(:opcode) { 0x8121 }
+
+      before do
+        emulator.general_registers[1] = 0x0011
+        emulator.general_registers[2] = 0x00AB
+      end
+
+      it "sets VX |= VY" do
+        expect(0x0011 | 0x00AB).not_to eq(0)
+        expect { subject }.to change { emulator.general_registers[1] }.to(0x0011 | 0x00AB)
+      end
+    end
+
+    describe '0x8XY2' do
+      let(:opcode) { 0x8122 }
+
+      before do
+        emulator.general_registers[1] = 0x0011
+        emulator.general_registers[2] = 0x00AB
+      end
+
+      it "sets VX &= VY" do
+        expect(0x0011 & 0x00AB).not_to eq(0)
+        expect { subject }.to change { emulator.general_registers[1] }.to(0x0011 & 0x00AB)
+      end
+    end
+
+    describe '0x8XY3' do
+      let(:opcode) { 0x8123 }
+
+      before do
+        emulator.general_registers[1] = 0x0011
+        emulator.general_registers[2] = 0x00AB
+      end
+
+      it "sets VX ^= VY" do
+        expect(0x0011 ^ 0x00AB).not_to eq(0)
+        expect { subject }.to change { emulator.general_registers[1] }.to(0x0011 ^ 0x00AB)
+      end
+    end
+
+    describe '0x8XY4' do
+      let(:opcode) { 0x8124 }
+
+      before do
+        emulator.general_registers[1] = 0x0011
+        emulator.general_registers[2] = 0x00AB
+      end
+
+      it "sets VX += VY" do
+        expect { subject }.to change { emulator.general_registers[1] }.to(0x0011 + 0x00AB)
+      end
+
+      context 'when sum is greater than 255' do
+        before do
+          emulator.general_registers[1] = 0xFF
+          emulator.general_registers[2] = 0x02
+        end
+
+        it "sets VX result is overflown result" do
+          expect { subject }.to change { emulator.general_registers[1] }.to(1)
+        end
+
+        it 'sets VF to 1' do
+          expect { subject }.to change { emulator.general_registers[0xF] }.from(0).to(1)
+        end
+      end
+    end
+
+    describe '0x8XY5' do
+      let(:opcode) { 0x8125 }
+
+      before do
+        emulator.general_registers[1] = 0x00AB
+        emulator.general_registers[2] = 0x0011
+      end
+
+      it "sets VX = VX - VY" do
+        expect { subject }.to change { emulator.general_registers[1] }.to(0x00AB - 0x0011)
+      end
+
+      it 'sets VF to 1' do
+        expect { subject }.to change { emulator.general_registers[0xF] }.from(0).to(1)
+      end
+
+      context 'when subtraction overflows' do
+        before do
+          emulator.general_registers[1] = 0x0011
+          emulator.general_registers[2] = 0x00AB
+          emulator.general_registers[0xF] = 1
+        end
+
+        it "sets VX result is overflown result" do
+          expect { subject }.to change { emulator.general_registers[1] }.to(0x0066)
+        end
+
+        it 'sets VF to 0' do
+          expect { subject }.to change { emulator.general_registers[0xF] }.from(1).to(0)
+        end
+      end
+    end
+
+    describe '0x8XY7' do
+      let(:opcode) { 0x8127 }
+
+      before do
+        emulator.general_registers[1] = 0x0011
+        emulator.general_registers[2] = 0x00AB
+      end
+
+      it "sets VX = VY - VX" do
+        expect { subject }.to change { emulator.general_registers[1] }.to(0x00AB - 0x0011)
+      end
+
+      it 'sets VF to 1' do
+        expect { subject }.to change { emulator.general_registers[0xF] }.from(0).to(1)
+      end
+
+      context 'when subtraction overflows' do
+        before do
+          emulator.general_registers[1] = 0x00AB
+          emulator.general_registers[2] = 0x0011
+          emulator.general_registers[0xF] = 1
+        end
+
+        it "sets VX result is overflown result" do
+          expect { subject }.to change { emulator.general_registers[1] }.to(0x0066)
+        end
+
+        it 'sets VF to 0' do
+          expect { subject }.to change { emulator.general_registers[0xF] }.from(1).to(0)
+        end
+      end
+    end
+
+    describe '0x8XY6' do
+      let(:opcode) { 0x8126 }
+
+      before { emulator.general_registers[1] = 0x0011 }
+
+      it "shifts VX to the right" do
+        expect { subject }.to change { emulator.general_registers[1] }.from(0x0011).to(0x0008)
+      end
+
+      it 'sets VF to 1' do
+        expect { subject }.to change { emulator.general_registers[0xF] }.from(0).to(1)
+      end
+
+      context 'when shifted out bit is 0' do
+        before do
+          emulator.general_registers[1] = 0x0010
+          emulator.general_registers[0xF] = 1
+        end
+
+        it "shifts VX to the right" do
+          expect { subject }.to change { emulator.general_registers[1] }.from(0x0010).to(0x0008)
+        end
+
+        it 'sets VF to 0' do
+          expect { subject }.to change { emulator.general_registers[0xF] }.from(1).to(0)
+        end
+      end
+    end
+
+    describe '0x8XYE' do
+      let(:opcode) { 0x812E }
+
+      before { emulator.general_registers[1] = 0x00FF }
+
+      it "shifts VX to the left" do
+        expect { subject }.to change { emulator.general_registers[1] }.from(0x00FF).to(0x00FE)
+      end
+
+      it 'sets VF to 1' do
+        expect { subject }.to change { emulator.general_registers[0xF] }.from(0).to(1)
+      end
+
+      context 'when shifted out bit is 0' do
+        before do
+          emulator.general_registers[1] = 0x0010
+          emulator.general_registers[0xF] = 1
+        end
+
+        it "shifts VX to the left" do
+          expect { subject }.to change { emulator.general_registers[1] }.from(0x0010).to(0x0020)
+        end
+
+        it 'sets VF to 0' do
+          expect { subject }.to change { emulator.general_registers[0xF] }.from(1).to(0)
+        end
+      end
+    end
+
     describe '0xANNN' do
       let(:opcode) { 0xA678 }
 
@@ -143,6 +464,30 @@ RSpec.describe Emulator::Emulator do
         subject
 
         expect(emulator.index_register).to eq(0x0678)
+      end
+    end
+
+    describe '0xBNNN' do
+      let(:opcode) { 0xB123 }
+
+      before { emulator.general_registers[0] = 0x0002 }
+
+      it 'sets program counter to NNN + V0' do
+        subject
+
+        expect(emulator.program_counter).to eq(0x0125)
+      end
+    end
+
+    describe '0xCXNN' do
+      # not the best code, but I'm too lazy to stub Kernel.rand
+      let(:opcode) { 0xC123 }
+
+      it 'sets VX to random number' do
+        subject
+
+        expect(emulator.general_registers[1]).to be_between(0, 0xFF)
+        expect(emulator.general_registers[1]).to eq(emulator.general_registers[1] & (opcode & 0x00FF))
       end
     end
 
@@ -179,6 +524,194 @@ RSpec.describe Emulator::Emulator do
 
       it 'sets VF to 0' do
         expect{ subject }.to change{ emulator.general_registers[0xF] }.from(1).to(0)
+      end
+
+      context 'when drawing out of bounds' do
+        let(:x_value) { 62 }
+        let(:y_value) { 31 }
+
+        before do
+          emulator.general_registers[0xF] = 0
+        end
+
+        it 'sets VF to 1' do
+          expect{ subject }.to change{ emulator.general_registers[0xF] }.from(0).to(1)
+        end
+      end
+    end
+
+    describe '0xEX9E' do
+      let(:opcode) { 0xE19E }
+
+      before do
+        emulator.general_registers[1] = 0x0A
+        emulator.pressed_key = 0x0A
+      end
+
+      it 'skips next instruction if key is pressed' do
+        expect{ subject }.to change{ emulator.program_counter }.by(2)
+      end
+
+      context 'when key is not pressed' do
+        before { emulator.pressed_key = 0x0B }
+
+        it 'does not skip next instruction' do
+          expect{ subject }.not_to change{ emulator.program_counter }
+        end
+      end
+    end
+
+    describe '0xEXA1' do
+      let(:opcode) { 0xE1A1 }
+
+      before do
+        emulator.general_registers[1] = 0x0A
+        emulator.pressed_key = 0x0B
+      end
+
+      it 'skips next instruction if key is not pressed' do
+        expect{ subject }.to change{ emulator.program_counter }.by(2)
+      end
+
+      context 'when key is pressed' do
+        before { emulator.pressed_key = 0x0A }
+
+        it 'does not skip next instruction' do
+          expect{ subject }.not_to change{ emulator.program_counter }
+        end
+      end
+    end
+
+    describe '0xFX07' do
+      let(:opcode) { 0xFE07 }
+
+      before { emulator.delay_timer = 0x0A }
+
+      it 'sets VX to delay timer' do
+        subject
+
+        expect(emulator.general_registers[0x0E]).to eq(0x0A)
+      end
+    end
+
+    describe '0xFX0A' do
+      let(:opcode) { 0xFE0A }
+
+      it 'waits for keypress' do
+        expect{ subject }.to change{ emulator.program_counter }.by(-2)
+      end
+
+      context 'when key is pressed' do
+        before { emulator.pressed_key = 0x0A }
+
+        it 'sets VX to pressed key' do
+          expect{ subject }.to change{ emulator.general_registers[0x0E] }.to(0x0A)
+        end
+      end
+    end
+
+    describe '0xFX15' do
+      let(:opcode) { 0xFE15 }
+
+      before { emulator.general_registers[0x0E] = 0x0A }
+
+      it 'sets delay timer to VX' do
+        expect{ subject }.to change { emulator.delay_timer }.to(0x0A)
+      end
+    end
+
+    describe '0xFX18' do
+      let(:opcode) { 0xFE18 }
+
+      before { emulator.general_registers[0x0E] = 0x0A }
+
+      it 'sets delay timer to VX' do
+        expect{ subject }.to change { emulator.sound_timer }.to(0x0A)
+      end
+    end
+
+    describe '0xFX1E' do
+      let(:opcode) { 0xFE1E }
+
+      before do
+        emulator.index_register = 0x0A
+        emulator.general_registers[0x0E] = 0x0B
+      end
+
+      it 'adds VX to index register' do
+        expect{ subject }.to change { emulator.index_register }.to(0x0A + 0x0B)
+      end
+
+      context 'when sum is greater than 0xFFFF' do
+        before do
+          emulator.index_register = 0xFFFE
+          emulator.general_registers[0x0E] = 0x0002
+        end
+
+        it 'adds VX to index register' do
+          expect{ subject }.to change { emulator.index_register }.to(0x0000)
+        end
+      end
+    end
+
+    describe '0xFX29' do
+      let(:opcode) { 0xFA29 }
+
+      it 'sets index register to font address' do
+        subject
+
+        expect(emulator.index_register).to eq(0x50 + 0x0A * 5)
+      end
+    end
+
+    describe '0xFX33' do
+      let(:opcode) { 0xFA33 }
+
+      before do
+        emulator.index_register = 0x0A
+        emulator.general_registers[0xA] = 123
+      end
+
+      it 'sets BCD representation of VX at index register' do
+        subject
+
+        expect(emulator.memory[0x0A]).to eq(1)
+        expect(emulator.memory[0x0B]).to eq(2)
+        expect(emulator.memory[0x0C]).to eq(3)
+      end
+    end
+
+    describe '0xFX55' do
+      let(:opcode) { 0xFA55 }
+
+      before do
+        emulator.index_register = 0x03
+        (0..0xA).each do |i|
+          emulator.general_registers[i] = i
+        end
+      end
+
+      it 'sets memory from index register to VX' do
+        subject
+
+        expect(emulator.memory[0x03..0x0D]).to eq((0..0xA).to_a)
+      end
+    end
+
+    describe '0xFX65' do
+      let(:opcode) { 0xFA65 }
+
+      before do
+        emulator.index_register = 0x03
+        (0..0xA).each do |i|
+          emulator.memory[0x03 + i] = i
+        end
+      end
+
+      it 'sets VX to memory from index register' do
+        subject
+
+        expect(emulator.general_registers[0..0xA]).to eq((0..0xA).to_a)
       end
     end
   end
